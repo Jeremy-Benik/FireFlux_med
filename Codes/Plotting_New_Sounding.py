96239e47f801b1050bc4a28a505f7ddd053459f7
Jeremy Benik <jeremybenik@Jeremys-MacBook-Pro.local>
2022-08-15 19:29:43 -0700

    Making new sounding excel sheets

--- Codes/Plotting_New_Sounding.py
+++ Codes/Plotting_New_Sounding.py
@@ -19,23 +19,31 @@ ws_old = df['ws_old']
 ws_new = df['ws_new']
 
 ws_adj = df['ws_adj']
-fig, ax = plt.subplots(1, 3, figsize = (12, 10))
 
-ax[0].plot(ws_old, ht_old, color = 'blue', label = 'Wind Speed')
+ws_new_new = df['ws_new_new']
+ht_new_new = df['ln_ht']
+
+ws_old_old = df['ws_old_old']
+ht_old_old = df['ln_ht_old']
+# %%
+fig, ax = plt.subplots(1, 2, figsize = (12, 10))
+
+ax[0].plot(ws_old_old, ht_old_old, color = 'blue', label = 'Wind Speed')
 ax[0].set_xlabel('Wind Speed (m/s)', fontsize = 12, fontweight = 'bold')
 ax[0].set_ylabel('LN Height', fontsize = 12, fontweight = 'bold')
 ax[0].set_title('Original Sounding', fontsize = 18, fontweight = 'bold')
 ax[0].grid()
 
-ax[1].plot(ws_new, ht_new, color = 'blue', label = 'Wind Speed')
+ax[1].plot(ws_new_new, ht_new_new, color = 'blue', label = 'Wind Speed')
 ax[1].set_xlabel('Wind Speed (m/s)', fontsize = 12, fontweight = 'bold')
 ax[1].set_ylabel('LN Height', fontsize = 12, fontweight = 'bold')
 ax[1].set_title('New Sounding', fontsize = 18, fontweight = 'bold')
 ax[1].grid()
 
-
+'''
 ax[2].plot(ws_adj, ht_new, color = 'blue', label = 'Wind Speed')
 ax[2].set_xlabel('Wind Speed (m/s)', fontsize = 12, fontweight = 'bold')
 ax[2].set_ylabel('LN Height', fontsize = 12, fontweight = 'bold')
 ax[2].set_title('Modified Winds Sounding', fontsize = 18, fontweight = 'bold')
 ax[2].grid()
+'''
